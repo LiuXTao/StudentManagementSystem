5f68cc13b40cec2e32b1e31ac41b38fb18390e05
zoharx <33194898+zoharx@users.noreply.github.com>
2017-11-03 19:25:17 +0800

    Update filesInfo.vue

--- src/components/pages/filesInfo.vue
+++ src/components/pages/filesInfo.vue
@@ -4,28 +4,28 @@
   <div class="clear"></div>
  <div class="el-main">
      <div id="infoform">
-<el-form id="info" :inline="true" :model="form" label-width="100px" >
+<el-form ref="form1" :inline="true" :model="form1" :rules="rule1" label-width="100px" >
      <el-form-item label="账号：">
-        <el-input v-model="form.pid" :disabled="true" style="width:140px" maxlength="15"></el-input>
+        <el-input v-model="form1.pid" :disabled="true" style="width:140px" ></el-input>
      </el-form-item>
     <el-form-item label="姓名：">
-        <el-input v-model="form.pname" :disabled="true" style="width:130px" maxlength="15"></el-input>
+        <el-input v-model="form1.pname" :disabled="true" style="width:130px" ></el-input>
      </el-form-item>
     <el-form-item label="性别：">
-       <el-select v-model="form.psex"  style="width:140px" placeholder="请选择性别">
+       <el-select v-model="form1.psex"  style="width:140px" placeholder="请选择性别">
       <el-option label=" " value="null"></el-option>
       <el-option label="男" value="male"></el-option>
       <el-option label="女" value="female"></el-option>
     </el-select>
      </el-form-item>
     <el-form-item label="学院：">
-        <el-input v-model="form.pdept" style="width:140px" :disabled="true"></el-input>
+        <el-input v-model="form1.pdept" style="width:140px" :disabled="true" ></el-input>
      </el-form-item>
     <el-form-item label="教龄：">
-        <el-input v-model="form.pteaage" style="width:50px" maxlength="3"></el-input>
+        <el-input v-model="form1.pteaage" type="number" style="width:100px" max="999"></el-input>
      </el-form-item>
     <el-form-item label="职务：">
-       <el-select v-model="form.ptitle" style="width:140px" placeholder="请选择职务">
+       <el-select v-model="form1.ptitle" style="width:140px" placeholder="请选择职务">
       <el-option label="  " value="null"></el-option>
       <el-option label="助教" value="assistant"></el-option>
       <el-option label="讲师" value="lecturer"></el-option>
@@ -34,38 +34,30 @@
     </el-select>
          </el-form-item>
      <el-form-item label="教育背景：">
-        <el-input v-model="form.peduback" maxlength="20"></el-input>
+        <el-input v-model="form1.peduback" auto-complete="off" type="text" maxlength="20"></el-input>
      </el-form-item>
          <el-form-item label="出生日期：" >
-        <el-date-picker v-model="form.pbirth" type="date" style="width:150px" placeholder="选择日期" :picker-options="pickerOptions"></el-date-picker>
+        <el-date-picker v-model="form1.pbirth" type="date" style="width:150px" placeholder="选择日期" :picker-options="pickerOptions"></el-date-picker>
      </el-form-item>
          <el-form-item label="民族：" >
-        <el-input v-model="form.pnation" style="width:140px"  maxlength="10"></el-input>
-     </el-form-item>
-     </el-form-item>
-         <el-form-item label="籍贯：" >
-        <el-input v-model="form.pnative" style="width:140px"  maxlength="10"></el-input>
-     </el-form-item>
-     </el-form-item>
-         <el-form-item label="国籍：" >
-        <el-input v-model="form.pcitizen" style="width:140px"  maxlength="10"></el-input>
+        <el-input v-model="form1.pnation" style="width:140px"  type="text" maxlength="10"></el-input>
      </el-form-item>
          <el-form-item label="联系方式：" >
-        <el-input v-model="form.pconinfo" style="width:160px" maxlength="15"></el-input>
+        <el-input v-model="form1.pconinfo" style="width:160px" atype="text" maxlength="15"></el-input>
      </el-form-item>
          <el-form-item label="政治面貌：">
-        <el-input v-model="form.ppolsta" style="width:100px" maxlength="10"></el-input>
+        <el-input v-model="form1.ppolsta" style="width:100px" type="text" maxlength="10"></el-input>
      </el-form-item>
          <el-form-item label="方向：">
-        <el-input v-model="form.pareainter" style="width:140px" maxlength="10"></el-input>
+        <el-input v-model="form1.pareainter" style="width:140px" type="text" maxlength="10" ></el-input>
      </el-form-item><el-col></el-col>
 </el-form>
-<el-form id="info2" :inline="true" :model="form" label-width="100px" >
+<el-form id="form2" :inline="true" label-width="100px" >
 <el-form-item>
-        <el-button v-model="modify" type="primary" @click="confirm" >保存</el-button>
+        <el-button type="primary" @click="submitForm" >保存</el-button>
      </el-form-item>
     <el-form-item>
-        <el-button v-model="rutern" type="primary" @click="handleReturn"> 返回</el-button>
+        <el-button type="primary" @click="handleReturn"> 返回</el-button>
      </el-form-item>
      </el-form>
         </div>
@@ -81,17 +73,130 @@
     components:{
               vHead
           },
-    data () {
+      data: function(){
+          var checklength1 = (rule,value,callback) => {
+            var vm = this.$data;
+            if( value.gblen() > 3){
+              vm.check6 = true;
+              return callback(new Error('长度不可超于3位'));
+           }else {
+              vm.check4 = false;
+           }
+        };
+          var checklength2 = (rule,value,callback) => {
+            var vm = this.$data;
+            if( value.gblen() > 20){
+              vm.check6 = true;
+              return callback(new Error('长度不可超于20个字符'));
+           }else {
+              vm.check4 = false;
+           }
+        };
+          var checklength3 = (rule,value,callback) => {
+            var vm = this.$data;
+            console.log(typeof value);
+            if( value.gblen() > 10){
+              vm.check6 = true;
+              return callback(new Error('长度不可超于10个字符'));
+           }else {
+              vm.check4 = false;
+           }
+        };
+          var checklength4 = (rule,value,callback) => {
+            var vm = this.$data;
+            console.log(typeof value);
+                           if(!value){
+                    vm.check1 = true;
+                    return callback(new Error('密码不能为空'));
+                }
+                else if( value.gblen() > 15){
+              vm.check6 = true;
+              return callback(new Error('长度不可超于15个字符'));
+           }else {
+              vm.check4 = false;
+           }
+        };
+          var checklength5 = (rule,value,callback) => {
+            var vm = this.$data;
+            console.log(typeof value);
+                            if(!value){
+                    vm.check1 = true;
+                    return callback(new Error('密码不能为空'));
+                }
+                else if( value.gblen() > 10){
+              vm.check6 = true;
+              return callback(new Error('长度不可超于10个字符'));
+           }else {
+              vm.check5 = false;
+           }
+        };
+          var checklength6 = (rule,value,callback) => {
+            var vm = this.$data;
+            var reg = /^[a-zA-Z0-9]+$/;
+            console.log(typeof value);
+                           if(!value){
+                    vm.check1 = true;
+                    return callback(new Error('密码不能为空'));
+                }
+                else if( value.gblen() > 10){
+              vm.check6 = true;
+              return callback(new Error('长度不可超于10个字符'));
+           }else {
+              vm.check6 = false;
+           }
+        };
       return {
+          hostUrl:'/teacher',
+          check1: false,
+          check2: false,
+          check3: false,
+          check4: false,
+          check5: false,
+          check6: true,
           pickerOptions: {
           disabledDate(time) {
             return time.getTime() > Date.now();
           }
         },
-          form:{
+          form1:{
               pid: '123456543248',
               pname: 'xxx',
-              pdept:'SAFDFAas'
+              pdept:'SAFDFAas',
+              pteaage:'',
+              peduback:'',
+              pbirth:'',
+              pnation:'',
+              pconinfo:'',
+              ppolsta:'',
+              pareainter:'',
+              psex:'',
+              ptitle:''
+          },
+          rule1:{
+              pteaage:[
+                { validator: checklength1, trigger:'change'},
+                { validator: checklength1, trigger:'blur'}
+            ],
+              peduback:[
+                { validator: checklength2, trigger:'change'},
+                { validator: checklength2, trigger:'blur'}
+            ],
+              pnation:[
+                { validator: checklength3, trigger:'change'},
+                { validator: checklength3, trigger:'blur'}
+            ],
+              pconinfo:[
+                { validator: checklength4, trigger:'change'},
+                { validator: checklength4, trigger:'blur'}
+            ],
+              ppolsta:[
+                { validator: checklength5, trigger:'change'},
+                { validator: checklength5, trigger:'blur'}
+            ],
+              pareainter:[
+                { validator: checklength6, trigger:'change'},
+                { validator: checklength6, trigger:'blur'}
+            ]
           }
       }
     },
@@ -141,31 +246,65 @@
                 }
             },
     handleReturn:function(){
-        this.$router.push('/');
+        this.$router.push('/teacher');
     },
     handleModify:function(key,keyPath){
               this.handleModify=key;
       console.log(this.handleModify);
     },
-    confirm() {
+    submitForm() {
+         var vm=this;
         this.$confirm('是否确定修改', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
+            var postData = {
+                            pteaage:"",
+                            peduback:"",
+                            pbirth:"",
+                            pnation:"",
+                            pconinfo:"",
+                            ppolsta:"",
+                            pareainter:"",
+                            psex:"",
+                            ptitle:""
+
+                        };
+                        postData.pteaage = vm.form1.pteaage;
+                        postData.peduback = vm.form1.peduback;
+                        postData.pbirth = vm.form1.pbirth;
+                        postData. pnation = vm.form1. pnation;
+                        postData.pconinfo = vm.form1.pconinfo;
+                        postData.ppolsta = vm.form1.ppolsta;
+                        postData.pareainter = vm.form1.pareainter;
+                        postData. psex = vm.form1. psex;
+                        postData.ptitle = vm.form1.ptitle;
+                         this.$http({
+                            url:'/filesInfo',
+                            method:'post',
+                            baseURL: vm.hostUrl,      
+                            data:postData
+                         }).then((response) => {
+                if(response.data.code == "200"){
           this.$message({
             type: 'success',
             message: '修改成功!'
           });
+            }else {
+                console.log(response.data.code);
+                vm.codeParsing(response.data.code);
+            }
         }).catch(() => {
           this.$message({
             type: 'info',
             message: '已取消修改'
-          });          
+          }) 
+        })         
         });
       }
-        }
     }
+  }
 </script>
 <style scoped>
 .clear{
@@ -181,11 +320,11 @@
 #infoform{
     padding-top: 60px;
 }
-#info{
+#form1{
     width:80%;
     margin:0px auto;
 }
-#info2{
+#form2{
     width:200px;
     margin:0px auto;
     float: center;
