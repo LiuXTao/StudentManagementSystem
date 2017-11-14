b6eba89fb1867a8b54a70517a2796a36742c0325
zoharx <33194898+zoharx@users.noreply.github.com>
2017-11-14 19:14:27 +0800

    Update filesInfo.vue

--- src/components/pages/filesInfo.vue
+++ src/components/pages/filesInfo.vue
@@ -4,28 +4,28 @@
   <div class="clear"></div>
  <div class="el-main">
      <div id="infoform">
-<el-form ref="form1" :inline="true" :model="form1" :rules="rule1" label-width="100px" >
+<el-form id="form1" ref="form1" :inline="true" :model="form1" :rules="rule1" label-width="100px" >
      <el-form-item label="账号：">
-        <el-input v-model="form1.pid" :disabled="true" style="width:140px" ></el-input>
+        <el-input v-model="form1.professor_id" :disabled="true" style="width:220px" ></el-input>
      </el-form-item>
     <el-form-item label="姓名：">
-        <el-input v-model="form1.pname" :disabled="true" style="width:130px" ></el-input>
+        <el-input v-model="form1.pname" :disabled="true" style="width:220px" ></el-input>
      </el-form-item>
     <el-form-item label="性别：">
-       <el-select v-model="form1.psex"  style="width:140px" placeholder="请选择性别">
+       <el-select v-model="form1.psex"   placeholder="请选择性别" style="width:220px">
       <el-option label=" " value="null"></el-option>
       <el-option label="男" value="male"></el-option>
       <el-option label="女" value="female"></el-option>
     </el-select>
      </el-form-item>
     <el-form-item label="学院：">
-        <el-input v-model="form1.pdept" style="width:140px" :disabled="true" ></el-input>
+        <el-input v-model="form1.pdepartment"  :disabled="true" style="width:220px"></el-input>
      </el-form-item>
-    <el-form-item label="教龄：">
-        <el-input v-model="form1.pteaage" type="number" style="width:100px" max="999"></el-input>
+    <el-form-item label="教龄：" prop="pteaching_age">
+        <el-input v-model="form1.pteaching_age" type="number"  max=999 style="width:220px"></el-input>
      </el-form-item>
-    <el-form-item label="职务：">
-       <el-select v-model="form1.ptitle" style="width:140px" placeholder="请选择职务">
+    <el-form-item label="职务：" >
+       <el-select v-model="form1.ptitle"  placeholder="请选择职务" style="width:220px">
       <el-option label="  " value="null"></el-option>
       <el-option label="助教" value="assistant"></el-option>
       <el-option label="讲师" value="lecturer"></el-option>
@@ -33,23 +33,20 @@
         <el-option label="教授" value="professor"></el-option>
     </el-select>
          </el-form-item>
-     <el-form-item label="教育背景：">
-        <el-input v-model="form1.peduback" auto-complete="off" type="text" maxlength="20"></el-input>
+     <el-form-item label="教育背景：" prop="peducational_background">
+        <el-input v-model="form1.peducational_background" auto-complete="off" style="width:220px" type="text" ></el-input>
      </el-form-item>
          <el-form-item label="出生日期：" >
-        <el-date-picker v-model="form1.pbirth" type="date" style="width:150px" placeholder="选择日期" :picker-options="pickerOptions"></el-date-picker>
+        <el-date-picker v-model="form1.pbirthday" type="date" style="width:220px" placeholder="选择日期" :picker-options="pickerOptions"></el-date-picker>
      </el-form-item>
-         <el-form-item label="民族：" >
-        <el-input v-model="form1.pnation" style="width:140px"  type="text" maxlength="10"></el-input>
+         <el-form-item label="民族：" prop="pnationality">
+        <el-input v-model="form1.pnationality" style="width:220px"  type="text"></el-input>
      </el-form-item>
-         <el-form-item label="联系方式：" >
-        <el-input v-model="form1.pconinfo" style="width:160px" atype="text" maxlength="15"></el-input>
+         <el-form-item label="政治面貌：" prop="ppolitics_status">
+        <el-input v-model="form1.ppolitics_status"  type="text" style="width:220px" ></el-input>
      </el-form-item>
-         <el-form-item label="政治面貌：">
-        <el-input v-model="form1.ppolsta" style="width:100px" type="text" maxlength="10"></el-input>
-     </el-form-item>
-         <el-form-item label="方向：">
-        <el-input v-model="form1.pareainter" style="width:140px" type="text" maxlength="10" ></el-input>
+         <el-form-item label="方向：" prop="parea_of_interest">
+        <el-input v-model="form1.parea_of_interest"  type="text" style="width:220px" ></el-input>
      </el-form-item><el-col></el-col>
 </el-form>
 <el-form id="form2" :inline="true" label-width="100px" >
@@ -76,124 +73,74 @@
       data: function(){
           var checklength1 = (rule,value,callback) => {
             var vm = this.$data;
-            if( value.gblen() > 3){
-              vm.check6 = true;
+            if( value.length > 3){
               return callback(new Error('长度不可超于3位'));
-           }else {
-              vm.check4 = false;
            }
         };
           var checklength2 = (rule,value,callback) => {
             var vm = this.$data;
-            if( value.gblen() > 20){
-              vm.check6 = true;
+            if( value.length > 20){
               return callback(new Error('长度不可超于20个字符'));
-           }else {
-              vm.check4 = false;
            }
         };
           var checklength3 = (rule,value,callback) => {
             var vm = this.$data;
-            console.log(typeof value);
-            if( value.gblen() > 10){
-              vm.check6 = true;
+            if( value.length > 10){
               return callback(new Error('长度不可超于10个字符'));
-           }else {
-              vm.check4 = false;
-           }
-        };
-          var checklength4 = (rule,value,callback) => {
-            var vm = this.$data;
-            console.log(typeof value);
-                           if(!value){
-                    vm.check1 = true;
-                    return callback(new Error('密码不能为空'));
-                }
-                else if( value.gblen() > 15){
-              vm.check6 = true;
-              return callback(new Error('长度不可超于15个字符'));
-           }else {
-              vm.check4 = false;
            }
         };
           var checklength5 = (rule,value,callback) => {
             var vm = this.$data;
             console.log(typeof value);
-                            if(!value){
-                    vm.check1 = true;
-                    return callback(new Error('密码不能为空'));
-                }
-                else if( value.gblen() > 10){
-              vm.check6 = true;
+            if( value.length > 10){
               return callback(new Error('长度不可超于10个字符'));
-           }else {
-              vm.check5 = false;
            }
         };
           var checklength6 = (rule,value,callback) => {
             var vm = this.$data;
-            var reg = /^[a-zA-Z0-9]+$/;
-            console.log(typeof value);
-                           if(!value){
-                    vm.check1 = true;
-                    return callback(new Error('密码不能为空'));
-                }
-                else if( value.gblen() > 10){
-              vm.check6 = true;
+            if( value.length > 10){
               return callback(new Error('长度不可超于10个字符'));
-           }else {
-              vm.check6 = false;
            }
         };
       return {
           hostUrl:'/teacher',
-          check1: false,
-          check2: false,
-          check3: false,
-          check4: false,
-          check5: false,
-          check6: true,
           pickerOptions: {
           disabledDate(time) {
             return time.getTime() > Date.now();
           }
         },
           form1:{
-              pid: '123456543248',
+              professor_id: '123456543248',
               pname: 'xxx',
-              pdept:'SAFDFAas',
-              pteaage:'',
-              peduback:'',
-              pbirth:'',
-              pnation:'',
-              pconinfo:'',
-              ppolsta:'',
-              pareainter:'',
+              pdepartment:'SAFDFAas',
+              pteaching_age:'',
+              peducation_background:'',
+              pbirthday: '',
+              pnationality: '',
+              pcontact_information: '',
+              ppolitics_status: '',
+              parea_of_interest: '',
               psex:'',
               ptitle:''
           },
           rule1:{
-              pteaage:[
+              pteaching_age:[
                 { validator: checklength1, trigger:'change'},
                 { validator: checklength1, trigger:'blur'}
             ],
-              peduback:[
+              peducational_background:[
                 { validator: checklength2, trigger:'change'},
                 { validator: checklength2, trigger:'blur'}
             ],
-              pnation:[
+              pnationality:[
                 { validator: checklength3, trigger:'change'},
                 { validator: checklength3, trigger:'blur'}
             ],
-              pconinfo:[
-                { validator: checklength4, trigger:'change'},
-                { validator: checklength4, trigger:'blur'}
-            ],
-              ppolsta:[
+              ppolitics_status:[
                 { validator: checklength5, trigger:'change'},
                 { validator: checklength5, trigger:'blur'}
             ],
-              pareainter:[
+              parea_of_interest:[
                 { validator: checklength6, trigger:'change'},
                 { validator: checklength6, trigger:'blur'}
             ]
@@ -260,24 +207,22 @@
           type: 'warning'
         }).then(() => {
             var postData = {
-                            pteaage:"",
-                            peduback:"",
-                            pbirth:"",
-                            pnation:"",
-                            pconinfo:"",
-                            ppolsta:"",
-                            pareainter:"",
-                            psex:"",
-                            ptitle:""
+                pteaching_age:"",
+                peducation_background:"",              
+                pbirthday: "",              
+                pnationality: "",                          
+                ppolitics_status:"",              
+                parea_of_interest:"",              
+                psex:"",              
+                ptitle:""
 
                         };
-                        postData.pteaage = vm.form1.pteaage;
-                        postData.peduback = vm.form1.peduback;
-                        postData.pbirth = vm.form1.pbirth;
-                        postData. pnation = vm.form1. pnation;
-                        postData.pconinfo = vm.form1.pconinfo;
-                        postData.ppolsta = vm.form1.ppolsta;
-                        postData.pareainter = vm.form1.pareainter;
+                        postData.pteaching_age = vm.form1.pteaching_age;
+                        postData.peducation_background = vm.form1.peducation_background;
+                        postData. pbirthday = vm.form1. pbirthday;
+                        postData. pnationality = vm.form1. pnationality;
+                        postData.ppolitics_status = vm.form1.ppolitics_status;
+                        postData.parea_of_interest = vm.form1.parea_of_interest;
                         postData. psex = vm.form1. psex;
                         postData.ptitle = vm.form1.ptitle;
                          this.$http({
